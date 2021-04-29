a0b5b707f5321c2956d6fd78011bfc53b005b231
kbaskerville <klbaskerille0520@gmail.com>
2021-04-29 10:44:41 -0700

    submaster

--- controllers/replies.js
+++ controllers/replies.js
@@ -7,13 +7,13 @@ const db = require("../models");
 
 // Rest Routes
 /*
- * Index - GET - /articles  - Presentational - respond with all articles
- * New - GET - /articles/new  - Presentational Form - a page with a form to create a new article
- * Show - GET - /articles/:id  - Presentational - respond with specific article by id
- * Create - Post - /articles  - Functional - recieve data from new route to create a article
- * Edit - GET - /articles/:id/edit  - Presentational Form - respond with a form prefilled with article data
- * Update - PUT - /articles/:id  - Functional - recieve data from edit to update a specific article
- * Delete - DELETE - /articles/:id  - Functional - Deletes article by id from request
+ * Index - GET - /replies  - Presentational - respond with all articles
+ * New - GET - /replies/new  - Presentational Form - a page with a form to create a new article
+ * Show - GET - /replies/:id  - Presentational - respond with specific article by id
+ * Create - Post - /replies  - Functional - recieve data from new route to create a article
+ * Edit - GET - /replies/:id/edit  - Presentational Form - respond with a form prefilled with article data
+ * Update - PUT - /replies/:id  - Functional - recieve data from edit to update a specific article
+ * Delete - DELETE - /replies/:id  - Functional - Deletes article by id from request
  */
 
 // Index
